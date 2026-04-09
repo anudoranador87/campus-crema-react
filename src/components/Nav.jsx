7f0bdad387d1af8f7b193f0a962ab2c98aa63beb
anudoranador87 <josemaparicio87@gmail.com>
2026-04-09 20:12:02 +0200

    Subiendo mi proyecto

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+    return (
+      <nav className="navbar">
+        <h2>Campus & Crema</h2>
+        <ul>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#carta">Carta</a></li>
+          <li><a href="#nosotros">Nosotros</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
